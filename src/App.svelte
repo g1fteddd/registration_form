31a12c18f0d7ed1728145bcf4808f04b77b38477
Rufat <mr.smashlincer@gmail.com>
2022-11-07 09:23:04 +0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script>
+	import Input from "./Input.svelte";
+
+
+
+</script>
+
+<div class="container">
+	<form action="" class="input-form">
+		<Input placeholder="E-mail" type="email"/>
+		<Input placeholder="Username"/>
+		<Input placeholder="Password" type="password"/>
+		<Input placeholder="Repeat the password" type="password"/>
+		<button type="submit">Register</button>
+	</form>
+</div>
+
+<style>
+	.container {
+		/* width: 50%;
+  		margin: 0 auto; */
+		width: 50vw;
+  		height: 50vh;
+  		background: red;
+		background-color: white;
+		border-radius: 20px
+	}
+
+	.input-form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+	:global(body) {
+  		display: flex;
+  		justify-content: center;
+  		align-items: center;
+  		height: 100%;
+	}
+
+	button {
+		margin-top: 20px;
+		background: #57b846;
+		border-radius: 50px;
+		width: 200px;
+		height: 40px;
+		color: #fff;
+		text-transform: uppercase;
+		font-family: Arial, Helvetica, sans-serif;
+		font-size: 15px;
+		font-weight: 700;
+	}
+</style>
---
+++ src/Input.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let placeholder = 'Введите значение';
+    export let type = "text";
+</script>
+
+
+<input class="input-form" type="{type}" placeholder="{placeholder}" />
+
+
+<style>
+    .input-form {
+        margin-top: 10px;
+        border-radius: 50px;
+        background: #e6e6e6;
+        color: #666;
+    }
+</style>
