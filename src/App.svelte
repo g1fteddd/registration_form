6500b58d9fec024bd8cd0050bdb0dd09caa63bcd
Руфат Сафиуллин <sr@certit.ru>
2022-11-07 14:06:23 +0500

    validation v1.0

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,99 @@
 <script>
-	import Input from "./Input.svelte";
+	import { afterUpdate, beforeUpdate, onMount } from 'svelte';
+	const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+	let email;
+	let username;
+	let password;
+	let repeatThePassword;
+	
+	let errorMessage;
+	
+	const checkEqualityPassword = () => password === repeatThePassword && password && repeatThePassword
+	
 
 
+	const isEmailValid = () => {
+		return EMAIL_REGEXP.test(email)
+	}
+
+	
+
+	let emailElement;
+	let usernameElement;
+	let passwordElement;
+	let repeatPasswordElement;
+	
+
+	
+
+	const register = (event) => {
+		event.preventDefault()
+
+		let isValidationPassed = false;
+
+		if (isEmailValid()) {
+			emailElement.style.borderColor = "green"
+			isValidationPassed = true
+		} else {
+			emailElement.style.borderColor = "red"
+			isValidationPassed = false
+		}
+		
+		if (checkEqualityPassword()) {
+			passwordElement.style.borderColor = "green"
+			repeatPasswordElement.style.borderColor = "green"
+			isValidationPassed = true
+		} else {
+			passwordElement.style.borderColor = "red"
+			repeatPasswordElement.style.borderColor = "red"
+			isValidationPassed = false
+		}
+
+		if (username) {
+			usernameElement.style.borderColor = "green"
+			isValidationPassed = true
+		} else {
+			usernameElement.style.borderColor = "red"
+			isValidationPassed = false
+		}
+
+		console.log(isValidationPassed)
+
+	}
+	
 
 </script>
 
 <div class="container">
 	<form action="" class="input-form">
-		<Input placeholder="E-mail" type="email"/>
-		<Input placeholder="Username"/>
-		<Input placeholder="Password" type="password"/>
-		<Input placeholder="Repeat the password" type="password"/>
-		<button type="submit">Register</button>
+		<input bind:this={emailElement} bind:value={email}  class="input-register" type="text" placeholder="E-mail" />
+		<input bind:this={usernameElement} bind:value={username} class="input-register" type="text" placeholder="Username" />
+		<input bind:this={passwordElement} bind:value={password} class="input-register" type="password" placeholder="Password" />
+		<input bind:this={repeatPasswordElement} bind:value={repeatThePassword} class="input-register" type="password" placeholder="Repeat the password" />
+		<button on:click={register} type="submit">Register</button>
+		<p></p>
 	</form>
 </div>
 
 <style>
 	.container {
-		/* width: 50%;
-  		margin: 0 auto; */
-		width: 50vw;
-  		height: 50vh;
+		min-width: 30%;
+  		margin: 0 auto;
+		/* width: 50vw;
+  		height: 50vh; */
   		background: red;
 		background-color: white;
 		border-radius: 20px
 	}
+
+	.input-register {
+        margin-top: 10px;
+        border-radius: 50px;
+        background: #e6e6e6;
+        color: #666;
+		outline: none;
+    }
 
 	.input-form {
         display: flex;
